e9d0e072dea39e1a339cbcd1fba2cac528bdb009
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2024-02-12 00:46:46 +0800

    v0.1.18: 实现禁漫的选择页码跳转功能

--- src/plugin_jm_server/static/js/common.js
+++ src/plugin_jm_server/static/js/common.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // 右侧菜单采用双击显示/隐藏
     const element = document.querySelector('.menu-bolock');
     document.addEventListener('dblclick', () => {
         if (element.style.display === 'none') {
@@ -8,9 +9,42 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-//     id=gobottom，对这个元素实现跳转到页面最底部
-    const bottom = document.getElementById('gobottom');
-    bottom.addEventListener('click', () => {
-        window.scrollTo(0, document.body.scrollHeight);
+    // 跳转到底部
+    document.getElementById('gobottom')
+        .addEventListener('click', () => {
+            window.scrollTo(0, document.body.scrollHeight);
+        });
+
+    // 选择页码跳转图片
+    document.getElementById('pageselect').addEventListener('change', function () {
+        let selectedValue = this.value;
+        if (selectedValue !== null) {
+            const divid = "page_" + selectedValue;
+            let element = document.getElementById(divid);
+            if (element) {
+                let settop = element.offsetTop - 28 - document.getElementById('Comic_Top_Nav').offsetHeight;
+                window.scrollTo(0, settop);
+            }
+            document.getElementById('pageselect').value = selectedValue;
+        }
     });
+
+    window.addEventListener('scroll', function () {
+        let wsTop = window.scrollY
+        let pageDivs = document.querySelectorAll('div[id*="page_"]');
+        let toPage = pageDivs.length - 1;
+
+        for (let div of pageDivs) {
+            let divTop = div.offsetTop;
+            if (divTop > wsTop) {
+                toPage = parseInt(div.getAttribute("data-page"));
+                break
+            }
+        }
+
+        console.log('toPage:', toPage)
+        document.getElementById('pageselect').value = toPage;
+    });
+
+
 });
